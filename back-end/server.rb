82435ad06e1cb28111c8bffbb851be3d3f89f518
Oliver <olliepar01@gmail.com>
2019-09-06 13:48:42 +0100

    Working two player ttt
    On page load the players both enther their names and if they have an account it will get there stats up and if they don't it will create them an account

--- back-end/server.rb
+++ back-end/server.rb
@@ -5,24 +5,33 @@ require_relative "./lib/TTT.rb"
 
 class Server < Sinatra::Base
 
-    get '/test' do
-        headers 'Access-Control-Allow-Origin' => '*'
-        david = User.find_by(user_name: "Tom")
-        return david.wins.to_s
-    end
-
     get '/userStats' do
         headers 'Access-Control-Allow-Origin' => '*'
         user = User.find_by(user_name: params[:name])
-        response = [user.user_name, user.wins.to_s, user.loses.to_s, user.games_played.to_s, user.last_played]
+        response = [user.id, user.user_name, user.wins.to_s, user.loses.to_s, user.games_played.to_s, user.last_played]
         return response.to_s
     end
 
-    get '/increaseWins' do  
+    get '/gameEnded' do  
         headers 'Access-Control-Allow-Origin' => '*'
-        user = User.find_by(user_name: params[:name])
-        user.wins += 1
-        user.save
+        winner = User.find_by(user_name: params[:winner])
+        loser = User.find_by(user_name: params[:loser])
+        winner.wins += 1
+        loser.loses += 1
+        winner.games_played += 1
+        loser.games_played += 1
+        winner.save
+        loser.save
+    end 
+
+    get '/draw' do  
+        headers 'Access-Control-Allow-Origin' => '*'
+        p1 = User.find_by(user_name: params[:p1])
+        p2 = User.find_by(user_name: params[:p2])
+        p1.games_played += 1
+        p2.games_played += 1
+        p1.save
+        p2.save
     end 
 
 end
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,31 +6,57 @@ import Board from './components/board'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {inputName: "", name: "", wins: null, loses: null};
+    this.state = {inputName1: "", inputName2: "", player1: [], player2: []};
   }
 
   componentDidMount() {
-    var name = prompt("What is your name?");
-    this.setState({inputName: name})
-    this.getUser(name);
+    var name1 = prompt("What is player 1's name?");
+    this.setState({inputName1: name1.toLowerCase})
+    var name2 = prompt("What is player 2's name?");
+    this.setState({inputName2: name2.toLowerCase})
+    this.getPlayer(name1, 1);
+    this.getPlayer(name2, 2);
   }
   
-  getUser(name) {
-    axios.get(`http://127.0.0.1:9393/userStats?name=${name}`)
-    .then(res => {
-      var result = res.data
-      console.log(result)
-      this.setState({name: result[0], wins: result[1], loses: result[2]})
-    })
+  getPlayer(name, player) {
+    if (player === 1) {
+      axios.get(`http://127.0.0.1:9393/userStats?name=${name}`)
+      .then(res => {
+        var result = res.data
+        console.log(result)
+        this.setState({player1: result})
+      })
+    }
+    if (player === 2) {
+      axios.get(`http://127.0.0.1:9393/userStats?name=${name}`)
+      .then(res => {
+        var result = res.data
+        console.log(result)
+        this.setState({player2: result})
+      })
+    }
   } 
 
   render() {
     return (
       <div>
-        <Board name={this.state.inputName} />
-        <p>{this.state.name}</p>
-        <p>{this.state.wins}</p>
-        <p>{this.state.loses}</p>
+        <Board player1={this.state.player1[1]} player2={this.state.player2[1]}/>
+        <div className="players">
+          <div className="player1">
+            <h2>Player 1</h2>
+            <p>Name: {this.state.player1[1]}</p>
+            <p>Wins: {this.state.player1[2]}</p>
+            <p>Loses: {this.state.player1[3]}</p>
+            <p>Games Played: {this.state.player1[4]}</p>
+          </div>
+          <div className="player2">
+            <h2>Player 2</h2>
+            <p>Name: {this.state.player2[1]}</p>
+            <p>Wins: {this.state.player2[2]}</p>
+            <p>Loses: {this.state.player2[3]}</p>
+            <p>Games Played: {this.state.player2[4]}</p>
+          </div>
+        </div>
       </div>
     )
   }

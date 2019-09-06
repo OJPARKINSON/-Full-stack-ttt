5d0685ed608200bb4038bf04c8e2298a7af62f2b
Oliver <olliepar01@gmail.com>
2019-09-06 14:26:31 +0100

    two player

--- src/App.js
+++ src/App.js
@@ -4,18 +4,71 @@ import './App.css';
 import Board from './components/board'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {inputName1: "", inputName2: "", player1: [], player2: []};
+  }
 
   componentDidMount() {
-    axios.get(`http://localhost:9393/test`)
-    .then(res => {
-      console.log(res.data);
-    })
+    var name1 = prompt("What is player 1's name?");
+    this.setState({inputName1: name1.toLowerCase})
+    var name2 = prompt("What is player 2's name?");
+    this.setState({inputName2: name2.toLowerCase})
+    this.getPlayer(name1, 1);
+    this.getPlayer(name2, 2);
   }
+  
+  getPlayer(name, player) {
+    if (player === 1) {
+      axios.get(`http://127.0.0.1:9393/userStats?name=${name}&type=old`)
+      .then(res => {
+        var result = res.data
+        console.log(result)
+        this.setState({player1: result})
+      })
+      .catch(function (error) {
+        axios.get(`http://127.0.0.1:9393/userStats?name=${name}&type=new`)
+        .then(
+          this.getPlayer(name, 1)
+        )
+      })
+    }
+    if (player === 2) {
+      axios.get(`http://127.0.0.1:9393/userStats?name=${name}&type=old`)
+      .then(res => {
+        var result = res.data
+        console.log(result)
+        this.setState({player2: result})
+      })
+      .catch(function (error) {
+        axios.get(`http://127.0.0.1:9393/userStats?name=${name}&type=new`)
+        .then(
+          this.getPlayer(name, 2)
+        )
+      })
+    }
+  } 
 
   render() {
     return (
       <div>
-        <Board />
+        <Board player1={this.state.player1[1]} player2={this.state.player2[1]}/>
+        <div className="players">
+          <div className="player1">
+            <h2>Player 1</h2>
+            <p>Name: {this.state.player1[1]}</p>
+            <p>Wins: {this.state.player1[2]}</p>
+            <p>Loses: {this.state.player1[3]}</p>
+            <p>Games Played: {this.state.player1[4]}</p>
+          </div>
+          <div className="player2">
+            <h2>Player 2</h2>
+            <p>Name: {this.state.player2[1]}</p>
+            <p>Wins: {this.state.player2[2]}</p>
+            <p>Loses: {this.state.player2[3]}</p>
+            <p>Games Played: {this.state.player2[4]}</p>
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/board.js
+++ src/components/board.js
@@ -1,31 +1,148 @@
 import React from 'react';
+import axios from 'axios';
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {squares: Array(9).fill(null)};
+        this.state = {squares: Array(9).fill(null), fc: 0, isNext: "O"};
+    }
+
+    componentDidMount() {
+        this.play()
+    }
+
+    play() {
+        this.setState({squares: Array(9).fill(null)})
+        this.emptyIndexies(this.state.squares);
     }
 
     renderSquare(i) {
-        return <button onClick={() => this.handleClick(i)}>{this.state.squares[i]}</button>
+        return <button onClick={() => this.handlePick(i)}>{this.state.squares[i]}</button>
     }
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-		if (this.winner(squares) || squares[i]) {
-			return;
+    handlePick(i) {
+        const squares = this.state.squares;
+		if (this.winner(squares)) {
+            console.log("Won")
+            return;
 		}
 
-		if(this.full(squares) === true) {
+		if(this.full(squares) === "X" || this.full(squares) === "O" ) {
+            console.log("full")
 			return;
-		}
-		
-		squares[i] = this.state.isNext ? 'X' : 'O';
+        }
+        var next = "";
+        squares[i] = this.state.isNext;
+
+        if (this.state.isNext === "O") {
+            next = "X";
+        } 
+        if (this.state.isNext === "X") {
+            next = "O";
+        }
 		this.setState({
 			squares: squares,
-			isNext: !this.state.isNext
-		});
+			isNext: next
+        });
+        
     }
+
+    // minimax(newBoard, player){
+    //     if (this.winner(this.state.squares)) {
+    //         return;
+    //     } else if(!this.winner(this.state.squares) && this.isFilled) {
+    //         return;
+    //     }
+
+    //     var updatedFC = this.state.fc + 1
+    //     this.setState({fc: updatedFC});
+    //     console.log(this.state.fc);
+    //     var availSpots = this.emptyIndexies(newBoard);
+    //     var huPlayer = "O";
+    //     var aiPlayer = "X";
+    //     if (this.winning(newBoard, huPlayer)){
+    //         console.log("Huwinning")
+    //        return {score:-10};
+    //     }
+    //     else if (this.winning(newBoard, aiPlayer)){
+    //         console.log("Aiwinning")
+    //       return {score:10};
+    //       }
+    //     else if (availSpots.length === 0){
+    //         console.log("winning")
+    //         return {score:0};
+    //     }
+    //     var moves = [];
+    //     for (var i = 0; i < availSpots.length; i++){
+    //         var move = {};
+    //         var result = 0;
+    //         newBoard[availSpots[i]] = player;
+    //         if (player === aiPlayer){
+    //             result = this.minimax(newBoard, huPlayer);
+    //             move.score = result.score;
+    //         }
+    //         else{
+    //         result = this.minimax(newBoard, aiPlayer);
+    //         move.score = result.score;
+    //         }
+        
+    //         newBoard[availSpots[i]] = move.index;
+    //         moves.push(move);
+    //     }
+    //     var bestMove;
+    //     if(player === aiPlayer){
+    //       var bestScore = -10000;
+    //       for(var i = 0; i < moves.length; i++){
+    //         if(moves[i].score > bestScore){
+    //           bestScore = moves[i].score;
+    //           bestMove = i;
+    //         }
+    //       }
+    //     }else{
+    //       var bestScore = 10000;
+    //       for(var i = 0; i < moves.length; i++){
+    //         if(moves[i].score < bestScore){
+    //           bestScore = moves[i].score;
+    //           bestMove = i;
+    //         }
+    //       }
+    //     }
+    //     console.log(moves[bestMove])
+    //     return moves[bestMove];
+    // }
+
+    full(squares) {
+        let count = 0;
+        squares.forEach(function (item) {
+            if (item !== null) {
+                count++;
+            }
+        });
+        if(count === 9) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // winning(board,player) {
+    //     if (
+    //         (board[0] === player && board[1] === player && board[2] === player) ||
+    //         (board[3] === player && board[4] === player && board[5] === player) ||
+    //         (board[6] === player && board[7] === player && board[8] === player) ||
+    //         (board[0] === player && board[3] === player && board[6] === player) ||
+    //         (board[1] === player && board[4] === player && board[7] === player) ||
+    //         (board[2] === player && board[5] === player && board[8] === player) ||
+    //         (board[0] === player && board[4] === player && board[8] === player) ||
+    //         (board[2] === player && board[4] === player && board[6] === player)
+    //         ) {
+    //         console.log("this user is winning")
+    //         return true;
+    //     } else {
+            
+    //         return false;
+    //     }
+    // }
 
     winner(squares) {
         const win_combination = [
@@ -49,20 +166,9 @@ class Board extends React.Component {
         return null;
     }
 
-    full(squares) {
-        let count = 0;
-        squares.forEach(function (item) {
-            if (item !== null) {
-                count++;
-            }
-        }); 
-        if(count === 9) {
-            return true;
-        } else {
-            return false;
-        }
+    emptyIndexies(board){
+        return board.filter(s => s !== "O" && s !== "X")
     }
-
 
   render() {
     const winner = this.winner(this.state.squares);
@@ -70,10 +176,16 @@ class Board extends React.Component {
     let status;
     if (winner) {
         status = 'The winner is: ' + winner;
-    } else if(!winner && isFilled) {
+        if (winner === "O") {
+            axios.get(`http://127.0.0.1:9393/gameEnded?winner=${this.props.player1}&loser=${this.props.player2}`);
+        } else {
+            axios.get(`http://127.0.0.1:9393/gameEnded?winner=${this.props.player2}&loser=${this.props.player1}`);
+        }
+    } else if(!this.winner(this.state.squares) && isFilled) {
         status = 'Game drawn';
+        axios.get(`http://127.0.0.1:9393/draw?p1=${this.props.player2}&p2=${this.props.player1}`);
     } else {
-        status = 'Now ' + (this.state.isNext ? 'X' : 'O') + '\'s turn';
+        status = 'Now ' + this.state.isNext + '\'s turn';
     }
 
     return (
@@ -94,6 +206,9 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <form action={this.emptyIndexies(this.state.squares)}>
+            <input type="submit" value="New game"/>
+        </form>
     </div>
     )
   }
